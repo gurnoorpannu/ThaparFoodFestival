739b452108d0133f39fb6d7cad5b0a9c7a067ef3
Gurnoor Singh Pannu <gurnxxrpannu@gmail.com>
2025-10-22 02:23:11 +0530

    added lottiefiles dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 }
 
 dependencies {
+    implementation("com.airbnb.android:lottie-compose:6.1.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -46,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
