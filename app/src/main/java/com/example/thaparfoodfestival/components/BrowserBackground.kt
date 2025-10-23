1b29384ca9ebe65f7ee5bb36a819bca81e23985a
Gurnoor Singh Pannu <gurnxxrpannu@gmail.com>
2025-10-24 05:28:58 +0530

    edited Browserbackground file

--- app/src/main/java/com/example/thaparfoodfestival/components/BrowserBackground.kt
+++ app/src/main/java/com/example/thaparfoodfestival/components/BrowserBackground.kt
@@ -1,10 +1,12 @@
 package com.example.thaparfoodfestival.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
